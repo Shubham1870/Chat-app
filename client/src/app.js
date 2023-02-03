026ecba78def148a9e45d5c3009287aaced77f84
shubham yadav <shubhr6566@gmail.com>
2023-02-03 13:18:57 +0530

    last update sign in and sign upp created

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,21 @@
 import React from "react";
-import{BrowserRouter,Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Join from "./components/Join/join"
 import Chat from "./components/Chat/chat";
-const App=()=>{
-    return(
+import Signin from "./components/Signin/Signin";
+import Signup from "./components/Signup/signup";
+const App = () => {
+    return (
         <>
-        <BrowserRouter>
-            <Routes>
-            <Route path="/" exact element={<Join/>}></Route>
-            <Route path="/chat" element={<Chat location={window.location}/>}></Route>
-        </Routes>
-        </BrowserRouter>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<Signin />}></Route>
+                    <Route path="/register" element={<Signup />}></Route>
+                    <Route  path="/login" element={<Signin />}></Route>
+                    <Route path="/join" element={<Join />}></Route>
+                    <Route path="/chat" element={<Chat location={window.location} />}></Route>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
