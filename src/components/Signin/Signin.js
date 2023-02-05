6ff110a520d8d2cab80f0ec7c944be188e9dae9f
Shuubham <107632088+Shubham1870@users.noreply.github.com>
2023-02-06 04:37:57 +0530

    Update Signin.js
    render url

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -10,7 +10,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/signin', {
+      const response = await fetch('https://chat-app-050m.onrender.com/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
      
